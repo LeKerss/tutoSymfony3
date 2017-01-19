44fb16678acd8dff5517de11ad578e0b7a487d03
LeKerss <annas9001@gmail.com>
2017-01-19 17:23:17 +0100

    chapter completed

---
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OC\PlatformBundle\Antispam;
+
+class OCAntispam {
+
+    private $mailer;
+    private $locale;
+    private $minLength;
+
+    public function __construct(\Swift_Mailer $mailer, $locale, $minLength) {
+        $this->mailer       = $mailer;
+        $this->locale       = $locale;
+        $this->minLength    = (int) $minLength;
+    }
+
+    /**
+     * Verifies whether text in parameter is spam
+     *
+     * @param string $text
+     * @return bool
+     */
+    public function isSpam($text) {
+        return strlen($text) < $this->minLength;
+    }
+}
